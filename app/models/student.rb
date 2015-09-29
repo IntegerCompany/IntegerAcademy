5095e62c8fdf14545fffdd08b3df3e8c67572660
yurii <mov4ko.y@gmail.com>
2015-09-29 17:35:23 +0300

    fixed bug with db table connects

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
 	has_many :sub_categories, through: :student_categories
+	has_many :student_categories
 end
--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,4 +1,5 @@
 class SubCategory < ActiveRecord::Base
   belongs_to :category
   has_many :students, through: :student_categories
+    has_many :student_categories
 end
